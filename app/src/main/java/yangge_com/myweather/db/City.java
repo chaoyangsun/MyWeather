5b91b96e2b11e06af2a5ec3fac659987b57d079e
孙朝阳 <1204113522@qq.com>
2017-10-19 14:36:25 +0800

    加入数据库的各项配置

---
+++ app/src/main/java/yangge_com/myweather/db/City.java
@@ -0,0 +1,46 @@
+package yangge_com.myweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by charming-yin on 2017/10/19.
+ */
+
+public class City extends DataSupport{
+    private int id;
+    private  String cityName;
+    private  int cityCode;
+    private  int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/yangge_com/myweather/db/Country.java
@@ -0,0 +1,47 @@
+package yangge_com.myweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by charming-yin on 2017/10/19.
+ */
+
+public class Country extends DataSupport{
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    private  String countryName;
+    private  String weatherId;
+    private  int cityId;
+}
